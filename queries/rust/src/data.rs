14a078b00b1614335fffbb2bf638ebfe911b440d
Long Ma <malongjordanl@126.com>
2025-03-21 15:19:41 +0100

    add components and change names of field to let rust match the name of wit and rust sturct

--- queries/rust/src/data.rs
+++ queries/rust/src/data.rs
@@ -1,13 +1,19 @@
 use runtime::prelude::*;
+use wasmtime::component::{ComponentType, Lower, Lift};
 
-#[data]
+#[derive(Debug, Clone, Send, DeepClone, serde::Serialize, serde::Deserialize, Timestamp, New, ComponentType, Lower, Lift)]
+#[serde(crate = "runtime::prelude::serde")]
+#[component(record)]
 pub struct Auction {
     pub id: u64,
+    #[component(name = "item-name")]
     pub item_name: String,
     pub description: String,
+    #[component(name = "initial-bid")]
     pub initial_bid: u64,
     pub reserve: u64,
     #[timestamp]
+    #[component(name = "date-time")]
     pub date_time: u64,
     pub expires: u64,
     pub seller: u64,
@@ -15,20 +21,27 @@ pub struct Auction {
     pub extra: String,
 }
 
-#[data]
+#[derive(Debug, Clone, Send, DeepClone, serde::Serialize, serde::Deserialize, Timestamp, New, ComponentType, Lower, Lift)]
+#[serde(crate = "runtime::prelude::serde")]
+#[component(record)]
 pub struct Person {
     pub id: u64,
     pub name: String,
+    #[component(name = "email-address")]
     pub email_address: String,
+    #[component(name = "credit-card")]
     pub credit_card: String,
     pub city: String,
     pub state: String,
     #[timestamp]
+    #[component(name = "date-time")]
     pub date_time: u64,
     pub extra: String,
 }
 
-#[data]
+#[derive(Debug, Clone, Send, DeepClone, serde::Serialize, serde::Deserialize, Timestamp, New, ComponentType, Lower, Lift)]
+#[serde(crate = "runtime::prelude::serde")]
+#[component(record)]
 pub struct Bid {
     pub auction: u64,
     pub bidder: u64,
@@ -36,6 +49,7 @@ pub struct Bid {
     pub channel: String,
     pub url: String,
     #[timestamp]
+    #[component(name = "date-time")]
     pub date_time: u64,
     pub extra: String,
 }
